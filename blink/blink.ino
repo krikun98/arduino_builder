f7dbfd9a388220ca4d8fc2157a09cf399b5e225a
krikun98 <krikun98@gmail.com>
2021-04-05 20:32:15 +0300

    push to build

--- blink/blink.ino
+++ blink/blink.ino
@@ -22,16 +22,18 @@
   http://www.arduino.cc/en/Tutorial/Blink
 */
 
+int LED_PIN=10;
+
 // the setup function runs once when you press reset or power the board
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
-  pinMode(10, OUTPUT);
+  pinMode(LED_PIN, OUTPUT);
 }
 
 // the loop function runs over and over again forever
 void loop() {
-  digitalWrite(10, HIGH);   // turn the LED on (HIGH is the voltage level)
+  digitalWrite(LED_PIN, HIGH);   // turn the LED on (HIGH is the voltage level)
   delay(1000);                       // wait for a second
-  digitalWrite(10, LOW);    // turn the LED off by making the voltage LOW
+  digitalWrite(LED_PIN, LOW);    // turn the LED off by making the voltage LOW
   delay(1000);                       // wait for a second
 }
